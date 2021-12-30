7369e0d8673a2313b2c1c0c6dda4406bea1e1ca0
Mr_nFuRoX <38051034+nfurox@users.noreply.github.com>
2021-12-30 15:13:06 +0000

    Update to fit my Server
    Update to fit my Server

--- banking_sql.sql
+++ banking_sql.sql
@@ -7,7 +7,25 @@ CREATE TABLE IF NOT EXISTS `society` (
 
 INSERT INTO `society` (`id`, `name`, `money`) VALUES
 	(1, 'police', '0'),
-	(2, 'lostmc', '0');
+	(2, 'thelost', '0'),
+	(3, 'ambulance', '0'),
+	(4, 'realestate', '0'),
+	(5, 'taxi', '0'),
+	(6, 'cardealer', '0'),
+	(7, 'mechanic', '0'),
+	(8, 'tuner', '0'),
+	(9, 'vagos', '0'),
+	(10, 'vanilla', '0'),
+	(11, 'tequilala', '0'),
+	(12, 'ballas', '0'),
+	(13, 'burgershot', '0'),
+	(14, 'ldmc', '0'),
+	(15, 'cookies', '0'),
+	(16, 'doink', '0'),
+	(17, 'catcafe', '0'),
+	(18, 'henhouse', '0'),
+	(19, 'pizza', '0'),
+	(20, 'gundealer', '0');
 
 CREATE TABLE IF NOT EXISTS `transaction_history` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
